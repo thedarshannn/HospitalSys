85eaac3b0e312a95f4172b6afbc1a10267abe400
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2025-10-14 15:20:09 -0400

    Initialize Spring Boot project for HospitalSys with essential configurations and dependencies.

---
+++ src/main/java/dev/darshan/hospitalsys/HospitalSysApplication.java
@@ -0,0 +1,13 @@
+package dev.darshan.hospitalsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/darshan/hospitalsys/HospitalSysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.darshan.hospitalsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
