590e3a1a0ca0e1c4b4f16a970085a20cb736b5ea
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2025-10-14 15:55:12 -0400

    Add `Patient` entity class for Hibernate ORM mapping.

---
+++ src/main/java/dev/darshan/hospitalsys/model/entity/Patient.java
@@ -0,0 +1,27 @@
+package dev.darshan.hospitalsys.model.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer patient_id;
+
+    private String first_name;
+
+    private String last_name;
+
+    private LocalDate date_of_birth;
+
+    private String gender;
+
+    private String phone_number;
+
+    private String address;
+
+    private Integer insurance_id;
+}
