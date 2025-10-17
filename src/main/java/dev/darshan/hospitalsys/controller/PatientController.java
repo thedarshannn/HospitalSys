3f9d1b0ba687c4c8e6c6d0b5bef46df414ca843b
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2025-10-17 16:47:31 -0400

    Added Controller and service class for patient REST API.

---
+++ src/main/java/dev/darshan/hospitalsys/controller/PatientController.java
@@ -0,0 +1,11 @@
+package dev.darshan.hospitalsys.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/patients")
+public class PatientController {
+}
---
+++ src/main/java/dev/darshan/hospitalsys/service/PatientService.java
@@ -0,0 +1,7 @@
+package dev.darshan.hospitalsys.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+}
